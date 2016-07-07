1c2c08cc27787cd87ffac4fbbd05e55d9303f9ef
Rumenigue <niguerag@hotmail.com>
2016-07-07 00:46:32 -0300

    DI Ok

--- ProjetoFidelidade.Model/Models/Cliente.cs
+++ ProjetoFidelidade.Model/Models/Cliente.cs
@@ -2,21 +2,21 @@
 
 namespace ProjetoFidelidade.Model
 {
-    public class Cliente
+    public sealed class Cliente
     {
         public Cliente()
         {
             DataInclusao = DateTime.Now;
         }
 
-        public int Id { get; set; }
-        public string Nome { get; set; }
-        public string CPF { get; set; }
-        public string Email { get; set; }
-        public string DddCelular { get; set; }
-        public string Celular { get; set; }
-        public Nullable<DateTime> DataCadastro { get; set; }
-        public DateTime DataInclusao { get; set; }
+        public int Id { get; private set; }
+        public string Nome { get; private set; }
+        public string CPF { get; private set; }
+        public string Email { get; private set; }
+        public string DddCelular { get; private set; }
+        public string Celular { get; private set; }
+        public Nullable<DateTime> DataCadastro { get; private set; }
+        public DateTime DataInclusao { get; private set; }
         public Nullable<DateTime> DataAlteracao { get; set; }
     }
 }
--- ProjetoFidelidade.WS/Global.asax.cs
+++ ProjetoFidelidade.WS/Global.asax.cs
@@ -1,11 +1,10 @@
-﻿using Autofac.Integration.WebApi;
-using ProjetoFidelidade.WS.App_Start;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using Autofac;
+using Autofac.Integration.WebApi;
+using ProjetoFidelidade.Data.Infrastructure;
+using ProjetoFidelidade.Data.Repositories;
+using ProjetoFidelidade.Service;
+using System.Reflection;
 using System.Web.Http;
-using System.Web.Routing;
 
 namespace ProjetoFidelidade.WS
 {
@@ -14,6 +13,24 @@ namespace ProjetoFidelidade.WS
         protected void Application_Start()
         {
             GlobalConfiguration.Configure(WebApiConfig.Register);
+
+            var builder = new ContainerBuilder();
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
+            builder.RegisterType<UnitOfWork>().As<IUnitOfWork>().InstancePerRequest();
+            builder.RegisterType<DbFactory>().As<IDbFactory>().InstancePerRequest();
+
+            // Repositories
+            builder.RegisterAssemblyTypes(typeof(ClienteRepository).Assembly)
+                .Where(t => t.Name.EndsWith("Repository"))
+                .AsImplementedInterfaces().InstancePerRequest();
+
+            // Services
+            builder.RegisterAssemblyTypes(typeof(ClienteService).Assembly)
+                .Where(t => t.Name.EndsWith("Service"))
+                .AsImplementedInterfaces().InstancePerRequest();
+
+            IContainer container = builder.Build();
+            GlobalConfiguration.Configuration.DependencyResolver = new AutofacWebApiDependencyResolver(container);
         }
     }
 }
